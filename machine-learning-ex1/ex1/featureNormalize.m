f2c3e2d645cfd0cb3929be311e880bc000af9914
rm1210 <zhanxcyy@gmail.com>
2017-03-13 21:41:14 +0800

    Finish: Linear regression with multiple variables

--- machine-learning-ex1/ex1/featureNormalize.m
+++ machine-learning-ex1/ex1/featureNormalize.m
@@ -24,10 +24,12 @@ sigma = zeros(1, size(X, 2));
 %               each feature. 
 %
 % Hint: You might find the 'mean' and 'std' functions useful.
-%       
-
-
-
+%
+for iter = 1:size(X, 2)
+  mu(iter) = mean(X(:,iter));
+  sigma(iter) = std(X(:,iter));
+  X_norm(:,iter) = (X(:,iter)-mu(iter))/sigma(iter);
+end
 
 
 
